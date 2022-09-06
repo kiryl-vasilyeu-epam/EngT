ce05d50787faa893db1ca6b0a1a97a3815d6a2dd
Kiryl Vasilyeu <Kiryl_Vasilyeu@epam.com>
2022-09-06 19:03:47 +0200

    fix: fix nan value

--- src/store/helpers.jsx
+++ src/store/helpers.jsx
@@ -59,5 +59,5 @@ export const getScore = (tasks) => {
     questionsCount += questions;
   });
 
-  return +((userScore / questionsCount) * 10).toFixed(1);
+  return +((userScore / questionsCount) * 10).toFixed(1) || 10;
 };
