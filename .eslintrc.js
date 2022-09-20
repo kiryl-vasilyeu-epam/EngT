ace5b71c722e6dce7d19ab86950f765b7c3f68bd
Kiryl Vasilyeu <Kiryl_Vasilyeu@epam.com>
2022-09-20 22:37:21 +0200

    feat: add config

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'react-app',
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  rules: {
+    'react/function-component-definition': 0,
+    'react/prop-types': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-props-no-spreading': 0,
+  },
+};
