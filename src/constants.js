9a54c8acc5f72c64157a50d9db7806d1321307a0
Kiryl Vasilyeu <Kiryl_Vasilyeu@epam.com>
2022-09-23 14:36:02 +0200

    ref: revert constants

--- src/constants.js
+++ src/constants.js
@@ -39,12 +39,12 @@ export const OKTO_TOKEN = `${
   '1w3Vd2vmQ41h93XEksD'.split('').reverse().join('')
 }`;
 
-// export const P_VAL = 'crewcabanger18';
-export const P_VAL = '1';
+export const P_VAL = 'crewcabanger18';
+// export const P_VAL = '1';
 
 export const NEW_LINE = '$NEW_LINE';
-// export const ENDPOINT = 'ws://44.202.28.38:8080/';
-export const ENDPOINT = 'ws://localhost:8080/';
+export const ENDPOINT = 'ws://44.202.28.38:8080/';
+// export const ENDPOINT = 'ws://localhost:8080/';
 
 export const CONTROL_PANEL = 'CONTROL_PANEL';
 export const USER_NAME = 'USER_NAME';
